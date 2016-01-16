6fbe9da608c842783aeb06354f08d589c69d7fa5
jhson5183 <jhson5183@gmail.com>
2016-01-16 23:22:05 +0900

    commit

--- app/src/main/java/com/jhson/imageload/parser/dom/ImageDomParser.java
+++ app/src/main/java/com/jhson/imageload/parser/dom/ImageDomParser.java
@@ -1,11 +1,12 @@
 package com.jhson.imageload.parser.dom;
 
 import com.jhson.imageload.model.ImageModel;
+import com.jhson.imageload.parser.BaseDomParser;
 
 /**
  * Created by jhson on 2016-01-16.
  */
-public class ImageDomParser extends BaseDomParser<ImageModel>{
+public class ImageDomParser extends BaseDomParser<ImageModel> {
 
     private final String TAG = "ImageDomParser";
 }

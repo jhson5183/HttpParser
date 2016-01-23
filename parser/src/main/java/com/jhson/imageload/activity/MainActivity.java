3300bd38f158e7cd3ba36bd52e3fa09037f98ec0
jhson5183 <jhson5183@gmail.com>
2016-01-23 23:20:39 +0900

    commit

---
+++ parser/src/main/java/com/jhson/imageload/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.jhson.imageload.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.jhson.imageload.R;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
---
+++ parser/src/main/java/com/jhson/imageload/task/ImageParsingTask.java
@@ -0,0 +1,62 @@
+package com.jhson.imageload.task;
+
+import android.content.Context;
+
+import com.jhson.imageload.connection.BaseConnection;
+import com.jhson.imageload.parser.BaseParser;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Http, parser를 컨트롤 하며 실제로 실행되는 쓰레드
+ * 사용자가 생성자를 통해 정의한 파서와 커넥션을 가지고 통신,파싱을 한다.
+ * Created by INT-jhson5183 on 2016. 1. 14..
+ */
+public class ImageParsingTask<Result> extends BaseAsyncTask<String, Void, List<Result>>{
+
+    private String TAG = "ImageParsingTask";
+
+
+    private BaseParser mBaseParser = null;
+    private BaseConnection mBaseConnection = null;
+
+    public ImageParsingTask(Context context, BaseParser baseParser, BaseConnection baseConnection){
+       super(context);
+        mBaseParser = baseParser;
+        mBaseConnection = baseConnection;
+    }
+
+    @Override
+    protected List<Result> doInBackground(String... params) {
+        return HttpParsingExecute(params[0], null);
+    }
+
+    private List<Result> HttpParsingExecute(String httpUrl, HashMap<String, String> map) {
+
+        InputStream is = mBaseConnection.getInputStream(httpUrl, map);
+        if(is == null){
+            return null;
+        }
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(is);
+
+        List<Result> model = null;
+        try {
+            model = mBaseParser.startParser(bufferedInputStream);
+            if(bufferedInputStream != null){
+                bufferedInputStream.close();
+            }
+            if(is != null){
+                is.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return model;
+
+    }
+
+}
